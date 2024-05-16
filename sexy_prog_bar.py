1c4525d716be1fe93849fe06e2cbde579ddb7e80
moyja <ariajyom@gmail.com>
2024-05-15 18:14:34 -0700

    Add files via upload

--- sexy_prog_bar.py
+++ sexy_prog_bar.py
@@ -1,8 +1,15 @@
 # @ moyja
 
+# I think I may need to do something so when i run this on gpu it doesnt have to spend all this time finding the global variables
+
 ########################################################################################################################
 
 import time
+
+########################################################################################################################
+
+MY_TIMES = {}
+PAST_POINTS = {}
 
 ########################################################################################################################
 
@@ -34,57 +41,58 @@ def clockface(seconds):
         return houstr + ':' + minstr + ':' + secstr
     
 def xbar(p, name = 'sexbar'):
-    # note that p must be zero to start
-    # should probably optimize for gpu by removing the calls to global variable except on updates
-    global pastpoint
+    if name not in PAST_POINTS:
+        PAST_POINTS[name] = 0
+
     L = 60
-    
     if p > 1 or p < 0:
         raise Exception('bar out of bounds')
-    elif p == 0:        
+    
+    if p <= PAST_POINTS[name]: 
         tic(name = name)
-        pastpoint = 0
-    elif L*p - pastpoint > .5:
-        pastpoint = int(rint(L*p))
+        PAST_POINTS[name] = p
+    elif int(L * p) > int(L * PAST_POINTS[name]):
+        PAST_POINTS[name] = p
+        tick = int(L * p)
         elapsed = toc(print_time = False, name = name)
         remaining = (1/p-1)*elapsed
         now_time = clockface(elapsed)
         fut_time = clockface(remaining)
-        bar = ' '*(7-len(now_time)) + now_time + '  '
-        if pastpoint == L:
-            print('\r' + bar + 'X' + ' '*(L-4) + chr(187) + '--X      ')
+        # bar = ' '*(7-len(now_time)) + now_time + '  '
+        bar = ' ' + now_time + '  '
+        
+        if tick >= L - 1:
+            print('\r' + bar + 'X' + ' '*(L-6) + chr(187) + '--X      ')
         else:
-            if pastpoint >= 4:
-                bar += '>' + ' '*(pastpoint-4) + chr(187) + '-->' + ' '*(L-1-pastpoint) + '<'
-            elif pastpoint == 3:
-                bar += '>' + '-->' + ' '*(L-4) + '<'
-            elif pastpoint == 2:
-                bar += '>' + '->' + ' '*(L-3) + '<'
-            elif pastpoint == 1:
-                bar += '>' + '>' + ' '*(L-2) + '<'
-            elif pastpoint == 0:
-                bar += '>' + ' '*(L-1) + '<'
+            if tick >= 4:
+                bar += '>' + ' '*(tick - 5) + chr(187) + '-->' + ' '*(L - 2 - tick) + '<'
+            elif tick == 3:
+                bar += '>' + '-->' + ' '*(L-5) + '<'
+            elif tick == 2:
+                bar += '>' + '->' + ' '*(L-4) + '<'
+            elif tick == 1:
+                bar += '>' + '>' + ' '*(L-3) + '<'
+            elif tick == 0:
+                bar += '>' + ' '*(L-2) + '<'
             print('\r' + bar + '  ' + fut_time + '    ', end ='\r')
 
-
 def tic(print_time = False, name = 'yommytime'):
-    global MY_TIMES
-    if 'MY_TIMES' not in globals():
-        MY_TIMES = {name : time.time()}
+    if name not in MY_TIMES:
+        MY_TIMES[name] = time.time()
 
     last_time = MY_TIMES[name]
     MY_TIMES[name] = time.time()
     time_diff =  MY_TIMES[name] - last_time
   
     if print_time:
-        print('TIME: ', name, ' :  delta  = ', time_diff)
+        print('TIME:', name, ': delta =', time_diff)
     return time_diff
     
 def toc(print_time = True, name = 'yommytime'):
     time_diff =  time.time() - MY_TIMES[name]
   
     if print_time:
-        print('TIME: ', name, ' :  delta  = ', time_diff)
+        print('TIME:', name, ': delta =', time_diff)
     return time_diff
 
 ########################################################################################################################
